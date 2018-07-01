d5e75c4e14ba0035fec2e26713d76f70519e6c39
serg201510 <serg201510@yandex.ru>
2018-07-01 23:58:57 +0300

    01.07.2018 23:59

---
+++ botvk/фракции.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace botvk
+{
+    class фракции
+    {
+        public string namefr;
+        public string teg;
+        public string spec;
+        public string uchast;
+        public string rang;
+    }
+/*
+    Фракция:
+1. Название
+2. Тег(3 заглавных символа)
+3. Специализация(напишу отдельно варианты)
+4. Участники(напишу отдельно структуру)
+5. Ранги(разделяются на рядовых офицеров и лидера), может быть разное кол-во, от 3 до 255
+*/
+}
